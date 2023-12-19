da89a230c312dc99dff8998ec8113ae52dc3c9cf
NIKHILLLLLLLLLL__ <mishranirajk121@gmail.com>
2023-12-19 14:48:58 +0530

    [DATABASE]added database for login page

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/login'
+db = SQLAlchemy(app)
+
+class detail(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=False,  nullable=False)
+    password = db.Column(db.String(8), unique=False, nullable=False)
+    
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+       
+        entry = detail(username=username, password=password)
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template('login.html')
+
+app.run(debug=True)
